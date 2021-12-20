7275ecbddbb00dc1f79672596780ff10914be44f
fernandesguilherme07 <guifhunter28@gmail.com>
2021-12-20 17:33:29 -0300

    contador

---
+++ tecnologias/JavaScript/introducaoJS/organizacao-de-projetos/assets/js/scripts.js
@@ -0,0 +1,14 @@
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    console.log("Fui clicado +!")
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    console.log("Fui clicado -!")
+}
